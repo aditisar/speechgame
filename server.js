16233dd44b391a806df5620727a0ca338dbf30b7
Aditi Sarkar <aditi0821@gmail.com>
2015-05-12 15:12:12 -0400

    removed unnecessary modules and old code

--- server.js
+++ server.js
@@ -8,45 +8,6 @@ var express = require('express'),
     sockets = require('./serverSocket.js');
 
 
-
-// var p1Port = new SerialPort('/dev/tty.usbmodem1411', {
-//    baudRate: 9600,
-//    // look for return and newline at the end of each data packet:
-//    parser: serialport.parsers.readline("\r\n")
-//  });
-
-
-// function showPortOpen() {
-//    console.log('port open. Data rate: ' + p1Port.options.baudRate);
-// }
-// function showPortClose() {
-//    console.log('port closed.');
-// }
-// function showError(error) {
-//    console.log('Serial port error: ' + error);
-// }
-// function parseP1Data(data) {
-//   console.log(data);
-//   if (data == 100) {
-//     console.log("Buzzer 1 pressed!")
-//     //Send a request to the client side to click buzzer 1. 
-//   }  
-
-//   if (data == 200) {
-//     console.log("Buzzer 2 pressed!");
-//     //Send a request to the client side to click buzzer 2. 
-//     // This might be moved to parseP2Data when we add another arduino connected to diff serial port
-//   }
-// }
-
-// p1Port.on('open', showPortOpen);
-// p1Port.on('close', showPortClose);
-// p1Port.on('error', showError);
-// p1Port.on('data', parseP1Data)
-
-//when you get client side data
-
-
 // Create a class that will be our main application
 var SimpleStaticServer = function() {
 
